5d593f2abfa4c845810ceb54b29e111d68b1e8c5
Eshaan Barkataki <39027204+Eshaancoding@users.noreply.github.com>
2020-10-24 19:09:52 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  AutoDiff
+//
+//  Created by Eshaan Arnav on 9/23/20.
+//
+#include "AutoDiff/AutoDiff.h"
+#include <stdlib.h>
+
+int main(int argc, const char * argv[]) {
+    Matrix x(2,2);
+    Variable j;
+    Variable y; 
+    Variable z;
+    Variable h;
+    x = {2,3,4,6};
+    cout<<x.array[0].value<<endl;
+    cout<<x.array[1].value<<endl;
+    cout<<x.array[2].value<<endl;
+    cout<<x.array[3].value<<endl;
+}
